bf2c8134a636ef1f23fa2cff99b91029719c2283
FixCrap <george_andriopoulos@hotmail.gr>
2025-04-15 10:26:02 +0300

    Implement roboto slab font in global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto_Slab } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +10,13 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const roboto_slab = Roboto_Slab({
+  weight: ["300", "400", "500", "600", "700"], // Include various weights
+  subsets: ["latin", "greek"], // Include Latin and Greek subsets
+  display: "swap", // Use 'swap' for better font loading performance
+  variable: "--font-roboto-slab", // Define the CSS variable name
 });
 
 export const metadata: Metadata = {
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`${geistSans.variable} ${geistMono.variable} ${roboto_slab.variable} antialiased`}>
         {children}
       </body>
     </html>
